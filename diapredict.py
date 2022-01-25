e84438cc6a2660f453982a0840885e8a600f50af
jassu622008 <78474277+jassu622008@users.noreply.github.com>
2022-01-25 21:39:52 +0530

    Update diapredict.py

--- diapredict.py
+++ diapredict.py
@@ -8,19 +8,18 @@ def main():
     try:
         df = pd.read_csv('diabetes.csv')
         Glucose = st.text_input('Enter Blood Glucose Level: ')
-        Insulin = st.text_input('Enter Insulin Level: ')
         Age = st.text_input('Enter Your Age: ')
 
         pre = linear_model.LinearRegression()
 
         pre.fit(df[['Glucose', 'Insulin', 'Age']], df.Outcome)
 
-        res = pre.predict([[Glucose, Insulin, Age]])
+        res = float(pre.predict([[Glucose,Age]]))
 
-        if float(res) <= 0:
+        if float(res) <= 0.0:
             st.title("It's Party Time,you doesn't have diabetes")
 
-        if float(res) >= 1:
+        if float(res) >= 1.0:
             st.title("Don't Worry You Have diabetes It Can Be cured or removed from you body by medicines")
 
     except ValueError:
